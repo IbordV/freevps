c664dd65a5288147b925533db65f1bdb7e281242
Ibord1967 <86087643+Ibord1967@users.noreply.github.com>
2021-09-26 11:07:46 +0700

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,21 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By Ibord" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user administrator @ibord#1967 /add >nul
+net localgroup administrators administrator /add >nul
+net user administrator /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant administrator:F >nul
+ICACLS C:\Windows\installer /grant administrator:F >nul
+echo Successfully been installed!, if the vps is expired, go make new one okay!
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get the ngrok tunnel, please make sure that NGROK_AUTH_TOKEN already correct in Settings> Secrets> Repository secret. Maybe the previous Virtual Machine still running: https://dashboard.ngrok.com/status/tunnels "
+echo Username: administrator
+echo Password: @ibord#1967
+echo Please login to your rdp now
+ping -n 10 127.0.0.1 >nul
